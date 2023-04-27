e473fc7446bcde7c9a641a37089509a3848b27cc
jiang_feng <fjiang44@usc.edu>
2023-04-27 16:56:15 -0700

    Done

--- stop-spring-boot-app.sh
+++ stop-spring-boot-app.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if pgrep -f "java -jar ./deploy/spring-petclinic-3.0.0-SNAPSHOT.jar" > /dev/null
-then
-    pkill -f "java -jar ./deploy/spring-petclinic-3.0.0-SNAPSHOT.jar"
-fi
+pkill -f 'java -jar ./deploy/spring-petclinic-3.0.0-SNAPSHOT.jar'

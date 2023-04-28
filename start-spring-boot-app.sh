9edddc5d1ac0d50007b5bbafddb43eb8a37290fc
jiang_feng <fjiang44@usc.edu>
2023-04-28 15:28:36 -0700

    Done

--- start-spring-boot-app.sh
+++ start-spring-boot-app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar ./deploy/spring-petclinic-3.0.0-SNAPSHOT.jar  > /dev/null 2>&1 &
+nohup java -jar /opt/spring-petclinic/spring-petclinic-3.0.0-SNAPSHOT.jar > /dev/null 2>&1 &

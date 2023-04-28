51ee9c8dcc551fcf20a6d736a7cfac41b4553f4f
jiang_feng <fjiang44@usc.edu>
2023-04-28 16:33:17 -0700

    Done

--- stop-spring-boot-app.sh
+++ stop-spring-boot-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Stopping Spring Boot application..."
-pkill -f 'java -jar ./deploy/spring-petclinic-3.0.0-SNAPSHOT.jar'
+pkill -f 'java -jar /opt/spring-petclinic/spring-petclinic-3.0.0-SNAPSHOT.jar'
 echo "Spring Boot application stopped."

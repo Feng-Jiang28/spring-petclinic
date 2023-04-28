25857224ea74c64545bc33da0fdb2f399a0c5b19
jiang_feng <fjiang44@usc.edu>
2023-04-28 16:17:33 -0700

    Done

--- start-spring-boot-app.sh
+++ start-spring-boot-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Spring Boot application..."
-nohup java -jar /opt/spring-petclinic/spring-petclinic-3.0.0-SNAPSHOT.jar > spring-boot.log 2>&1 &
+nohup java -jar /opt/spring-petclinic/spring-petclinic-3.0.0-SNAPSHOT.jar > /opt/spring-petclinic/spring-boot.log 2>&1 &
 echo "Spring Boot application started."

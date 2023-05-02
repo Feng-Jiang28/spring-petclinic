3d8d8b9bd10ec68807a02ed16c62a1645ff9bc36
jiang_feng <fjiang44@usc.edu>
2023-05-02 15:37:13 -0700

    Done

--- deploy/install-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install java-1.8.0-openjdk
--- deploy/stop-spring-boot-app.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pkill -f 'java -jar /opt/spring-petclinic/spring-petclinic-3.0.0-SNAPSHOT.jar' || true

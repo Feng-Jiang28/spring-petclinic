b36723f1d28b725cecde47477357b86c50877925
jiang_feng <fjiang44@usc.edu>
2023-05-03 00:24:56 -0700

    Done

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://petclinic-webappdeploymentbucket-17b0rrpj6ceak/my-petclinic.war /opt/tomcat/webapps/my-petclinic.war
+aws s3 cp s3://petclinic-webappdeploymentbucket-17b0rrpj6ceak/spring-petclinic-3.0.0-SNAPSHOT.war /opt/tomcat/webapps/spring-petclinic-3.0.0-SNAPSHOT.war
 
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /opt/tomcat/webapps/

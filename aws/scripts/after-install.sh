9c409368576c5ce334f124267530d39855b8f0a9
jiang_feng <fjiang44@usc.edu>
2023-05-03 09:51:43 -0700

    Done

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xe
 
+# Create the 'tomcat' user and group if they do not already exist
+if ! id -u tomcat > /dev/null 2>&1; then
+    sudo groupadd tomcat
+    sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+fi
+
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://petclinic-webappdeploymentbucket-17b0rrpj6ceak/spring-petclinic-3.0.0-SNAPSHOT.war /opt/tomcat/webapps/spring-petclinic-3.0.0-SNAPSHOT.war
 
